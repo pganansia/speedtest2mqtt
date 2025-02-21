e2b356621f28ff47c36583d1817a3a4b1f8c29f1
Pierre <66196530+pganansia@users.noreply.github.com>
2025-02-21 15:18:42 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,10 +15,8 @@ fi
 # Copie des fichiers de configuration vers /config
 ##################################################################
 CRON=${CRON:-0 0,6,12,18 * * *}
-declare | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /var/tmp/container.env
-#sed -i "/schedule/c\    schedule: \"${CRON}\"" /app/config/crontab.yml
+declare | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /config/container.env
 sed -i "/schedule/c\    schedule: \"${CRON}\"" /config/crontab.yml
 echo "starting cron (${CRON})"
-#/yacronenv/bin/yacron -c /app/config/crontab.yml
 /yacronenv/bin/yacron -c /config/crontab.yml
 echo "speedtest2mqtt.sh has been planned "
