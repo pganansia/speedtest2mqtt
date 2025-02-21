7fef6ad6ee01e7d86013571ad1322b25563b6aa7
Pierre <66196530+pganansia@users.noreply.github.com>
2025-02-21 15:00:27 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-CRON=${CRON:-0 0,6,12,18 * * *}
-echo "speedtest2mqtt has been started "
+##################################################################
+# Copie des fichiers de configuration vers /config
+##################################################################
+if [ ! -f "/config/crontab.yml" ]; then
+    echo "Copying crontab.yml ..."
+    cp /app/config/crontab.yml /config
+fi
+if [ ! -f "/config/speedtest2mqtt.sh" ]; then
+    echo "Copying speedtest2mqtt.sh ..."
+    cp /app/config/speedtest2mqtt.sh /config
+fi
 
+##################################################################
+# Copie des fichiers de configuration vers /config
+##################################################################
+CRON=${CRON:-0 0,6,12,18 * * *}
 declare | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /var/tmp/container.env
 sed -i "/schedule/c\    schedule: \"${CRON}\"" /app/config/crontab.yml
-
+#sed -i "/schedule/c\    schedule: \"${CRON}\"" /config/crontab.yml
 echo "starting cron (${CRON})"
 /yacronenv/bin/yacron -c /app/config/crontab.yml
+#/yacronenv/bin/yacron -c /config/crontab.yml
+echo "speedtest2mqtt.sh has been planned "
