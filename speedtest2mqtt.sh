534278a04cf5bab96a1b98df34b3ff761d1d7d82
Pierre <66196530+pganansia@users.noreply.github.com>
2025-03-05 11:58:36 +0100

    Modification speedtest2mqtt.sh

--- speedtest2mqtt.sh
+++ speedtest2mqtt.sh
@@ -20,8 +20,13 @@ echo "${TIMESTAMP} Result file: ${FILE_RESULT}"
 
 #############################################
 # Lancement de speedtest.
-#############################################                                                                                      
+#############################################
 speedtest --accept-license --accept-gdpr -f json-pretty > ${FILE_RESULT}
+
+#############################################
+# Récupération des résultats.
+#############################################
+echo "${TIMESTAMP} Speedtest results"
 
 #############################################
 # Test d'un resultat en erreur.
@@ -29,8 +34,11 @@ speedtest --accept-license --accept-gdpr -f json-pretty > ${FILE_RESULT}
 nberror=$(jq -r '.error' ${FILE_RESULT} | wc -w)
 echo "${TIMESTAMP} Erreur de traitement nb mots: ${nberror}"
 if [ ${nberror} -gt 1 ]; then
-    echo "${TIMESTAMP} error in results";
-    exit;
+    echo "${TIMESTAMP} Results with errors"
+    error=$(jq -r '.error' ${FILE_RESULT})
+    echo "${TIMESTAMP} Speedtest results"
+    echo "${TIMESTAMP} error               = ${error}"
+    exit 1
 fi
 
 #############################################
@@ -41,49 +49,48 @@ jitter=$(jq -r '.ping.jitter' ${FILE_RESULT})
 ping=$(jq -r '.ping.latency' ${FILE_RESULT})
 download=$(printf %.2f\\n "$(($(jq -r '.download.bandwidth' ${FILE_RESULT}) * 8))e-6")
 upload=$(printf %.2f\\n "$(($(jq -r '.upload.bandwidth' ${FILE_RESULT}) * 8))e-6")
-isp=$(jq -r '.isp' ${FILE_RESULT})                                                    
-interfaceinternalip=$(jq -r '.interface.internalIp' ${FILE_RESULT})                   
-interfacename=$(jq -r '.interface.name' ${FILE_RESULT})                               
-interfacemacaddr=$(jq -r '.interface.macAddr' ${FILE_RESULT})                         
-interfaceisvpn=$(jq -r '.interface.isVpn' ${FILE_RESULT})                             
-interfaceexternalip=$(jq -r '.interface.externalIp' ${FILE_RESULT})                   
-servername=$(jq -r '.server.name' ${FILE_RESULT})                                     
-servercountry=$(jq -r '.server.country' ${FILE_RESULT})                               
-serverlocation=$(jq -r '.server.location' ${FILE_RESULT})                             
-serverhost=$(jq -r '.server.host' ${FILE_RESULT})                                     
-serverport=$(jq -r '.server.port' ${FILE_RESULT})                                     
-serverip=$(jq -r '.server.ip' ${FILE_RESULT})                                         
-resultid=$(jq -r '.result.id' ${FILE_RESULT})                                         
-resulturl=$(jq -r '.result.url' ${FILE_RESULT})                                       
-                                                                               
+isp=$(jq -r '.isp' ${FILE_RESULT})
+interfaceinternalip=$(jq -r '.interface.internalIp' ${FILE_RESULT})
+interfacename=$(jq -r '.interface.name' ${FILE_RESULT})
+interfacemacaddr=$(jq -r '.interface.macAddr' ${FILE_RESULT})
+interfaceisvpn=$(jq -r '.interface.isVpn' ${FILE_RESULT})
+interfaceexternalip=$(jq -r '.interface.externalIp' ${FILE_RESULT})
+servername=$(jq -r '.server.name' ${FILE_RESULT})
+servercountry=$(jq -r '.server.country' ${FILE_RESULT})
+serverlocation=$(jq -r '.server.location' ${FILE_RESULT})
+serverhost=$(jq -r '.server.host' ${FILE_RESULT})
+serverport=$(jq -r '.server.port' ${FILE_RESULT})
+serverip=$(jq -r '.server.ip' ${FILE_RESULT})
+resultid=$(jq -r '.result.id' ${FILE_RESULT})
+resulturl=$(jq -r '.result.url' ${FILE_RESULT})
+
 #############################################
 # Affichage des resultats.
 #############################################
-echo "${TIMESTAMP} Speedtest results"                                          
-echo "${TIMESTAMP} timestamp           = ${timestamp}"                                   
-echo "${TIMESTAMP} jitter              = ${jitter} ms"                                  
-echo "${TIMESTAMP} ping                = ${ping} ms"                                          
-echo "${TIMESTAMP} download            = ${download} Mbps"                                
-echo "${TIMESTAMP} upload              = ${upload} Mbps"                                    
-echo "${TIMESTAMP} isp                 = ${isp}"                                               
-echo "${TIMESTAMP} interfaceinternalip = ${interfaceinternalip}"           
+echo "${TIMESTAMP} timestamp           = ${timestamp}"
+echo "${TIMESTAMP} jitter              = ${jitter} ms"
+echo "${TIMESTAMP} ping                = ${ping} ms"
+echo "${TIMESTAMP} download            = ${download} Mbps"
+echo "${TIMESTAMP} upload              = ${upload} Mbps"
+echo "${TIMESTAMP} isp                 = ${isp}"
+echo "${TIMESTAMP} interfaceinternalip = ${interfaceinternalip}"
 echo "${TIMESTAMP} interfacename       = ${interfacename}"
-echo "${TIMESTAMP} interfacemacaddr    = ${interfacemacaddr}"                     
-echo "${TIMESTAMP} interfaceisvpn      = ${interfaceisvpn}"                         
-echo "${TIMESTAMP} interfaceexternalip = ${interfaceexternalip}"           
-echo "${TIMESTAMP} servername          = ${servername}"                                 
-echo "${TIMESTAMP} serverlocation      = ${serverlocation}"                         
-echo "${TIMESTAMP} servercountry       = ${servercountry}"                           
-echo "${TIMESTAMP} serverhost          = ${serverhost}"                                 
-echo "${TIMESTAMP} serverport          = ${serverport}"                             
+echo "${TIMESTAMP} interfacemacaddr    = ${interfacemacaddr}"
+echo "${TIMESTAMP} interfaceisvpn      = ${interfaceisvpn}"
+echo "${TIMESTAMP} interfaceexternalip = ${interfaceexternalip}"
+echo "${TIMESTAMP} servername          = ${servername}"
+echo "${TIMESTAMP} serverlocation      = ${serverlocation}"
+echo "${TIMESTAMP} servercountry       = ${servercountry}"
+echo "${TIMESTAMP} serverhost          = ${serverhost}"
+echo "${TIMESTAMP} serverport          = ${serverport}"
 echo "${TIMESTAMP} serverip            = ${serverip}"
 echo "${TIMESTAMP} resultid            = ${resultid}"
 echo "${TIMESTAMP} resulturl           = ${resulturl}"
 echo "${TIMESTAMP} sending results to ${MQTT_HOST} as clientID ${MQTT_ID} with options ${MQTT_OPTIONS} using user ${MQTT_USER}"
 
-#############################################                    
-# Envoi des valeurs vers MQTT.                         
-#############################################                                                                                      
+#############################################
+# Envoi des valeurs vers MQTT.
+#############################################
 /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/timestamp -m "${timestamp}"
 /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/jitter -m "${jitter}"
 /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/ping -m "${ping}"
@@ -103,3 +110,9 @@ echo "${TIMESTAMP} sending results to ${MQTT_HOST} as clientID ${MQTT_ID} with o
 /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/server/ip -m "${serverip}"
 /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/result/id -m "${resultid}"
 /usr/bin/mosquitto_pub -h ${MQTT_HOST} -i ${MQTT_ID} ${MQTT_OPTIONS} -u ${MQTT_USER} -P ${MQTT_PASS} -t ${MQTT_TOPIC}/result/url -m "${resulturl}"
+
+#############################################
+# Ending Speedtest.
+#############################################
+echo "${TIMESTAMP} Ending speedtest"
+exit 0
